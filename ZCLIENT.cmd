8035a91653d989a11b17678e8d9c101a6c92b1f2
Christian Brunner <31685593+prsbrc@users.noreply.github.com>
2018-10-24 08:21:01 +0200

    Add values OBJTYPE

--- ZCLIENT.cmd
+++ ZCLIENT.cmd
@@ -1,15 +1,29 @@
-             CMD        PROMPT('Send objects') TEXT('Send objects')
+             CMD        PROMPT('Send objects / IBMi') TEXT('Send objects to another IBMi')
 
              PARM       KWD(OBJ) TYPE(OBJLIB) MIN(1) CHOICE('Objectname') +
 			            PROMPT('Objectname')
 
-             PARM       KWD(OBJTYPE) TYPE(*CHAR) LEN(10) DFT(*ALL) +
+             PARM       KWD(OBJTYPE) TYPE(*CHAR) LEN(10) + 
+			              DFT(*ALL) VALUES(*ALL *ALRTBL *BNDDIR +
+						  *CHTFMT *CLD *CLS *CMD *CRG *CRQD *CSI +
+                          *CSPMAP *CSPTBL *DTAARA *DTAQ *EDTD +
+                          *EXITRG *FCT *FILE *FNTRSC *FNTTBL +
+                          *FORMDF *FTR *GSS *IGCDCT *IGCSRT *IGCTBL +
+                          *IMGCLG *JOBD *JOBQ *JOBSCD *JRN *JRNRCV +
+                          *LIB *LOCALE *MEDDFN *MENU *MGTCOL +
+                          *MODULE *MSGF *MSGQ *NODGRP *NODL *NWSCFG +
+                          *OUTQ *OVL *PAGDFN *PAGSEG *PDFMAP *PDG +
+                          *PGM *PNLGRP *PRDAVL *PRDDFN *PRDLOD +
+                          *PSFCFG *QMFORM *QMQRY *QRYDFN *RCT *SBSD +
+                          *SCHIDX *SPADCT *SQLPKG *SQLUDT *SQLXSR +
+						  *SRVPGM *SSND *SVRSTG *S36 *TBL *TIMZON +
+                          *USRIDX *USRQ *USRSPC *VLDL *WSCST) +
                           CHOICE('Objecttype, *ALL') +
                           PROMPT('Objecttype')
 
              PARM       KWD(HOST) TYPE(*CHAR) LEN(16) +
                           CHOICE('IP-Adress or Hostname') +
-                          PROMPT('Server-Adress')
+                          PROMPT('Server-Address')
 
              PARM       KWD(USER) TYPE(*NAME) LEN(10) +
                           CHOICE('Username') +
